456c700310c1ea1a543d960c3df7dd7eb4544900
Thingo713 <215013446@stu.ukzn.ac.za>
2023-06-11 02:00:10 +0200

    Added a div to show active reservations, so a guest can cancel. Not functional yet.

--- HotelManagementSystem/GuestDashboard.aspx.designer.cs
+++ HotelManagementSystem/GuestDashboard.aspx.designer.cs
@@ -15,6 +15,33 @@ namespace HotelManagementSystem
     {
 
         /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// Button4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
+
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
